5656ba68148d2ce0c85c74793704ad8ef0bcc3a6
Peng QiBiao <andreamapp@sina.com>
2019-12-05 13:36:54 +0800

    [Add] web terminal based on jquery.terminal

--- server.js
+++ server.js
@@ -32,6 +32,18 @@ if(fs.existsSync('host')) {
     host = fs.readFileSync('host');
 }
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1')
+  if(req.method == 'OPTIONS') {
+      res.sendStatus(200); 
+  } else { 
+      next(); 
+  }
+});
+
 app.get('/telcmd', function(req, res, next) {
     let cmd = req.query.cmd;
     let name = req.query.name;
